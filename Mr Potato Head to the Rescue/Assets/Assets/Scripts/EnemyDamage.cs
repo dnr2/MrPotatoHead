b59cabe750422309bc464703fc6e69d5911b8359
dnr2 <dnr2@cin.ufpe.br>
2014-12-03 01:10:03 -0200

    finalizado enemy demage, criado script que cuida do HP do inimigo e das animacoes de hit e de death, acoplar a um collider que e trigger (exemplo nos droids da scene1

--- Mr Potato Head to the Rescue/Assets/Assets/Scripts/EnemyDamage.cs
+++ Mr Potato Head to the Rescue/Assets/Assets/Scripts/EnemyDamage.cs
@@ -1,17 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/*
+ * Usar esse script em um objeto com collider e trigger ativado
+ * Colocar a referencia do Objeto raiz do inimigo em enemyReference
+ */
+
+
 public class EnemyDamage : MonoBehaviour {
 
-	public float totalHP;
+	public int life = 1; // Vida do inimigo
+	public int playerDamage = 1; // dano causado TODO: isso deve mudar pois cada arma causa dano diferente
 	public GameObject enemyReference;
-	private bool dead;
+	private bool isDead = false;
 
-
-	/*
-	void onTriggerEnter( GameObject colider )
-	{
+	void Start(){
 
 	}
-	*/
+
+	void Update(){
+		if (isDead && audio != null && !audio.isPlaying) {
+			Destroy(enemyReference);
+		}
+	}
+
+	//TODO inportar o script com as informacoes das armas, para saber quanto de dano elas infligem.
+	void OnTriggerEnter( Collider collider )
+	{
+		if (collider.gameObject.tag == "Player") {
+			life -= playerDamage;
+			runHitAnimation();			
+
+			if( life <= 0 ){
+				runDeathAnimation();
+				isDead = true;
+			}
+		}
+	}
+
+	void runHitAnimation(){
+
+		//ira tentar tocar Audio Source componente (ver documentacao do Unity)
+		if (audio != null) {
+			audio.Play();		
+		}
+	}
+
+	void runDeathAnimation(){
+
+		//TODO adicionar alguma forma para animar o personagem e deleta-lo depois que ele morrer
+	}
+
 }
