ebd2263d876015bfa2b3d96e9955f78bdc45f39d
dnr2 <dnr2@cin.ufpe.br>
2014-12-02 22:59:25 -0200

    inicio do script de dano no inimigo

---
+++ Mr Potato Head to the Rescue/Assets/Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyDamage : MonoBehaviour {
+
+	public float totalHP;
+	public GameObject enemyReference;
+	private bool dead;
+
+
+	/*
+	void onTriggerEnter( GameObject colider )
+	{
+
+	}
+	*/
+}
