3aea4085ef53568ab4b67059e2c447c37f237439
Antonio Nobrega <amngcin@gmail.com>
2014-12-06 23:33:42 -0200

    Checkpoint script adicionado!

---
+++ Mr Potato Head to the Rescue/Assets/Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckPoint : MonoBehaviour {
+
+	public Transform spawnPoint;
+	public AudioClip checkpointAudio;
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Player") {
+			//audio.Play();
+			spawnPoint.position = new Vector3(transform.position.x, spawnPoint.position.y, spawnPoint.position.z);
+			Object.Destroy(gameObject);
+
+			}
+		}
+	}
