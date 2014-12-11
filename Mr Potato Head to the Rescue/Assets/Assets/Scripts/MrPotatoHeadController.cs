fbcca33759900b2f620815333872db78775fcc41
dnr2 <dnr2@cin.ufpe.br>
2014-12-11 00:21:39 -0200

    ataque do chapeu inicial

--- Mr Potato Head to the Rescue/Assets/Assets/Scripts/MrPotatoHeadController.cs
+++ Mr Potato Head to the Rescue/Assets/Assets/Scripts/MrPotatoHeadController.cs
@@ -1,22 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Boundary{
-	
-}
 
 public class MrPotatoHeadController : MonoBehaviour 
 {
-	public float speed;
-
-	void FixedUpdate ()
-	{
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = 0.0f; // so vai ser alterado ao pular
-
-		Vector3 movement = new Vector3 (moveHorizontal, moveVertical, 0.0f);
-		rigidbody.velocity = movement * speed;
-
-	}
+	
 
 }
---
+++ Mr Potato Head to the Rescue/Assets/Assets/Scripts/ThrowHat.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrowHat : MonoBehaviour {
+
+	private Animator animator;
+	private bool attacking;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+		animator.SetBool("isAttack", false);
+		attacking = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(! attacking && Input.GetKeyDown(KeyCode.Z)) {
+			animator.SetBool("isAttack", true);
+			attacking = true;
+		}
+		if (animator.GetCurrentAnimatorStateInfo (0).IsName ("AttackHat")) {
+			animator.SetBool("isAttack", false);
+			attacking = false;
+		}
+	}
+}
