b3d8246ef9b4db7909da5f9cdd9a5ce753a31efd
caiosfonseca <csf2@cin.ufpe.br>
2015-02-07 16:29:14 -0300

    Conserta a camera

--- Mr Potato Head to the Rescue/Assets/Assets/Scripts/MainCameraController.cs
+++ Mr Potato Head to the Rescue/Assets/Assets/Scripts/MainCameraController.cs
@@ -6,9 +6,13 @@ public class MainCameraController : MonoBehaviour {
 	public Transform target;
 	public float distance = 3.0f;
 	public float height = 3.0f;
+	public float width = 3.0f;
 	public float damping = 5.0f;
 	public bool smoothRotation = true;
 	public float rotationDamping = 10.0f;
+	public float thresholdYtop = 5.0f;
+	public float thresholdYbot = 10.0f;
+	public float thresholdX = 10.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -18,8 +22,26 @@ public class MainCameraController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Vector3 newPosition = target.position;
+
+		if (newPosition.y - transform.position.y > thresholdYtop) {
+			newPosition.y = newPosition.y - thresholdYtop;
+		} else if (newPosition.y - transform.position.y < (-1)*thresholdYbot){
+			newPosition.y = newPosition.y + thresholdYbot;
+		}
+		else {
+			newPosition.y = transform.position.y;
+		}
+				
+		if (newPosition.x - transform.position.x > thresholdX) {
+			newPosition.x = newPosition.x - thresholdX;
+		} else if (newPosition.x - transform.position.x < (-1)*thresholdX){
+			newPosition.x = newPosition.x + thresholdX;
+		}
+		else {
+			newPosition.x = transform.position.x;
+		}
 		newPosition.z -= distance;
-		newPosition.y += height;
+
 		transform.position = newPosition;
 	}
 }
