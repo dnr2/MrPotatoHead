b21217620e386026f860541819a268afe7d1e229
caiosfonseca <csf2@cin.ufpe.br>
2015-02-07 17:48:55 -0300

    Adicionando o Zurg, Missil teleguiado e bola de tenis

---
+++ Mr Potato Head to the Rescue/Assets/Assets/Scripts/TennisBallController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TennisBallController : MonoBehaviour {
+	public Vector3 targetPos;
+	private Vector3 startPos;
+	public float speed = 30f;
+	private Vector3 direction;
+	private float maxDistance = 200;
+	
+	// Use this for initialization
+	void Start () {
+		startPos = this.transform.position;
+		direction = targetPos - this.transform.position;
+		direction = Vector3.Normalize(direction);
+		this.rigidbody.velocity = direction * speed;
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.GetComponent<TennisBallController>() == null 
+		   || other.gameObject.GetComponent<ProjectileController>() == null)
+			Destroy(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Vector3.Distance(this.transform.position, startPos) > maxDistance)
+			Destroy(gameObject);
+	}
+}
---
+++ Mr Potato Head to the Rescue/Assets/Assets/Scripts/ZurgController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZurgController : MonoBehaviour {
+	public GameObject tennisBall;
+	public GameObject heatSeekingMissile;
+	public GameObject flyingPurpleET;
+	public GameObject shotSpawn;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
