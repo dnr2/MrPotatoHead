0d37514632a6583963911310e46b66b30023a5cf
dnr2 <dnr2@cin.ufpe.br>
2015-02-18 23:44:37 -0200

    crido tiro do slime

---
+++ Mr Potato Head to the Rescue/Assets/SlimeController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlimeController : MonoBehaviour {
+
+	private float nextFire;
+	public float fireRate = 5f;
+	public GameObject slimeShot;
+
+	// Use this for initialization
+	void Start () {
+		nextFire = Time.time + fireRate;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > nextFire ) {
+			Debug.Log( "atirando slime" );
+			Instantiate( slimeShot, gameObject.transform.position,  Quaternion.identity );
+			nextFire = Time.time + fireRate;
+		}
+	}
+}
---
+++ Mr Potato Head to the Rescue/Assets/SlimeShotController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlimeShotController : MonoBehaviour {
+
+	public float speed = 30f;
+
+	// Use this for initialization
+	void Start () {
+		Vector3 direction = new Vector3 (0, -1, 0);
+		this.rigidbody.velocity =  direction * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Destroy(gameObject);
+	}
+
+}
